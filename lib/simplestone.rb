90a164919565034c3b49121a6d1677c9e4c2700c
Luke Abbott <banjerluke@gmail.com>
2009-12-31 01:57:18 -0800

    Load routes after main app routes, using a Thoughtbot technique: http://robots.thoughtbot.com/post/159805560

--- lib/simplestone.rb
+++ lib/simplestone.rb
@@ -1,5 +1,21 @@
-
 lib_dir = File.join(File.dirname(__FILE__))
 Dir.glob([File.join(lib_dir,"simplestone/*.rb"), File.join(lib_dir,"simplestone/**/*.rb")]).each {|file| require file }
+
+# I know, this is *ugly*, but until Rails gives us an elegant way
+# of doing this, it's necessary to keep Simplestone's routes from
+# overriding the main application routes in config/routes.rb
+# 
+class ActionController::Routing::RouteSet
+  def load_routes_with_simplestone!
+    simplestone_routes = File.join(File.dirname(__FILE__), 
+                       *%w[.. config simplestone_routes.rb])
+    unless configuration_files.include? simplestone_routes
+      add_configuration_file(simplestone_routes)
+    end
+    load_routes_without_simplestone!
+  end
+
+  alias_method_chain :load_routes!, :simplestone
+end
 
 puts "Simplestone loaded..."
